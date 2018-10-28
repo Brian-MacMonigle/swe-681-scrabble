3bc03aee68c0ffb3338b962d26ef4c1e9583abb9
brian-macmonigle <brian.macmonigle@gmail.com>
2018-10-29 00:19:45 +0100

    added initial Board structure

--- src/Server/Board/Board.js
+++ src/Server/Board/Board.js
@@ -1,3 +1,62 @@
+const tile = {
+	letterPoints: 1,
+	wordPoints: 1,
+	letter: " ",
+	startTile: false
+};
+
+const doubleLetterTile = {
+	...tile,
+	letterPoints: 2
+};
+
+const tripleLetterTile = {
+	...tile,
+	letterPoints: 3
+};
+
+const doubleWordTile = {
+	...tile,
+	wordPoints: 2
+};
+
+const tripleWordTile = {
+	...tile,
+	wordPoints: 3
+};
+
+const startTile = {
+	...tile,
+	startTile: true
+};
+
+const tileTypes = [
+	tile,
+	doubleLetterTile,
+	tripleLetterTile,
+	doubleWordTile,
+	tripleWordTile,
+	startTile
+];
+
 export function newBoard() {
 	return {};
+}
+
+export function isValidTile(tile) {
+	// ensure tile is one of the predefined types
+	const { letterPoints, wordPoints, letter, startTile } = tile;
+	if (
+		!letter ||
+		(typeof letter === "string" && letter.match(/^[a-z ]$/gm) === null)
+	) {
+		return false;
+	}
+
+	return tileTypes.some(
+		type =>
+			type.letterPoints === letterPoints &&
+			type.wordPoints === wordPoints &&
+			type.startTile === startTile
+	);
 }
--- src/Server/Board/Board.test.js
+++ src/Server/Board/Board.test.js
@@ -1,15 +1,75 @@
-import * as Board from './Board';
+import * as Board from "./Board";
 
-describe('Board', () => {
+describe("Board", () => {
 	let newBoard;
 	beforeAll(() => {
-		console.log(Board);
 		newBoard = Board.newBoard();
 	});
 
-	describe('newBoard()', () => {
-		it('matches snapshot', () => {
+	describe("newBoard()", () => {
+		it("matches snapshot", () => {
 			expect(newBoard).toMatchSnapshot();
+		});
+	});
+
+	describe("isValidTile()", () => {
+		it("matches normal tile", () => {
+			const tile = {
+				letterPoints: 1,
+				wordPoints: 1,
+				letter: " ",
+				startTile: false
+			};
+			expect(Board.isValidTile(tile)).toEqual(true);
+		});
+
+		it("succeeds for adding a key", () => {
+			const badTile = {
+				letterPoints: 1,
+				wordPoints: 1,
+				letter: " ",
+				startTile: false,
+				somethingExtra: "should cause function to fail"
+			};
+			expect(Board.isValidTile(badTile)).toEqual(true);
+		});
+
+		it("fails for removing a key", () => {
+			const badTile = {
+				letterPoints: 1,
+				wordPoints: 1,
+				letter: " "
+			};
+			expect(Board.isValidTile(badTile)).toEqual(false);
+		});
+
+		it("fails for making a required key undefined", () => {
+			const badTile = {
+				letterPoints: 1,
+				wordPoints: 1,
+				startTile: false
+			};
+			expect(Board.isValidTile(badTile)).toEqual(false);
+		});
+
+		it("fails for invalid (non-alpha or space) letter", () => {
+			const badTile = {
+				letterPoints: 1,
+				wordPoints: 1,
+				letter: "1",
+				startTile: false
+			};
+			expect(Board.isValidTile(badTile)).toEqual(false);
+		});
+
+		it("fails for modifying a valid tile", () => {
+			const badTile = {
+				letterPoints: 3,
+				wordPoints: 3,
+				letter: " ",
+				startTile: false
+			};
+			expect(Board.isValidTile(badTile)).toEqual(false);
 		});
 	});
 });
