4f9d9cf42877f7eb8589d7c3cdc752dca80eba98
brian-macmonigle <brian.macmonigle@gmail.com>
2018-10-30 04:17:35 +0100

    fix heroku build issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const Board = require("./Board");
+const Board = require("./src/Board");
 
 const app = express();
 const port = process.env.PORT || 5000;
